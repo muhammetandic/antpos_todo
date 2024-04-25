06562511ff593a5ffc65cbb4c2b7515ded553b2e
Muhammet Andiç <muhammet.andic@gmail.com>
2024-04-25 15:18:33 +0300

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginPrettier from "eslint-plugin-prettier";
+
+export default [
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+  {
+    ignores: ["**/node_modules/**", "**/dist/**"],
+  },
+  {
+    rules: {
+      curly: "warn",
+    },
+  },
+];
