08f3ce2068461cf72310f1e019ab68c43f88012c
Muhammet Andiç <muhammet.andic@gmail.com>
2024-05-04 19:49:38 +0300

    error handler refactored

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,11 +1,6 @@
 import { NextFunction, Request, Response } from "express";
-import { MongoDbError } from "../value-objects/errors.js";
 
-export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
-  if (err instanceof MongoDbError) {
-    console.log(`[${err.name}]: ${err.message}`);
-    res.status(500).json({ type: err.name, error: err.message });
-  } else {
-    next();
-  }
+export async function errorHandler(err: Error, _req: Request, res: Response, _next: NextFunction) {
+  console.log(`[${err.name}]: ${err.message}`);
+  return res.status(500).json({ type: err.name, error: err.message });
 }
