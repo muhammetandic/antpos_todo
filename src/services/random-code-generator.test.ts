82a4ef3e1ecb8936e2d667cbcc5819698a6ca5d2
Muhammet Andiç <muhammet.andic@gmail.com>
2024-05-07 13:38:48 +0300

    test configuration and first test for random-code-generator added

---
+++ src/services/random-code-generator.test.ts
@@ -0,0 +1,16 @@
+import { createCode } from "./random-code-generator.js";
+
+describe("code generator", () => {
+  it("should generate six digit code in string form", () => {
+    const code = createCode();
+    expect(code.length).toBe(6);
+  });
+  it("should'nt generate null value", () => {
+    const code = createCode();
+    expect(code).not.toBeNull();
+  });
+  it("should generate only numbers", () => {
+    const code = createCode();
+    expect(code).toMatch(/^\d+$/);
+  });
+});
