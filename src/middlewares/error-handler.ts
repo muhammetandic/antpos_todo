c1b7f3665a1af55fe43afb6bcc87793db0c10018
Muhammet Andiç <muhammet.andic@gmail.com>
2024-08-26 00:01:11 +0300

    structure fixed for helper folder

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
+import { status } from "../helpers/response.js";
 
 export async function errorHandler(err: Error, _req: Request, res: Response, _next: NextFunction) {
   console.log(`[${err.name}]: ${err.message}`);
-  return res.status(500).json({ type: err.name, error: err.message });
+  return res.status(status.InternalServerError).json({ type: err.name, error: err.message });
 }
